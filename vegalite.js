c58092b10a20ce402b98625bda6b6807b0b810fd
Mark Riego <mrie0002@student.monash.edu>
2023-10-12 15:57:44 +1100

    yes

--- vegalite.js
+++ vegalite.js
@@ -2,3 +2,8 @@ var patent = "patent_radar_chart.json";
 vegaEmbed("#radar", patent).then(function(result) {
 
 }).catch(console.error);
+
+var superchargers = "supercharger_dot_map.json";
+vegaEmbed("#dotmap", superchargers).then(function(result) {
+
+}).catch(console.error);
