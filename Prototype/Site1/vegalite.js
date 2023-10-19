c7542e2638d6589f004ede3d35dfb6f61d0cce28
Mark Riego <mrie0002@student.monash.edu>
2023-10-19 13:29:32 +1100

    lol

---
+++ Prototype/Site1/vegalite.js
@@ -0,0 +1,9 @@
+var patent = "patent_radar_chart.json";
+vegaEmbed("#radar", patent).then(function(result) {
+
+}).catch(console.error);
+
+var charger = "supercharger_dot_map.json";
+vegaEmbed("#map", charger).then(function(result) {
+
+}).catch(console.error);
